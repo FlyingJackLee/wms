e6ef7edb1a8b54a1e8d17c109b573db14057d848
Li Zumin <zumin.li.work@gmail.com>
2023-12-05 17:41:54 +0800

    shopping dev 0.5

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ShoppingComponent } from "./views/shopping/shopping.component"
+import { InventoryComponent } from "./views/inventory/inventory.component"
+import { StatisticsComponent } from "./views/statistics/statistics.component"
+
+
+const routes: Routes = [
+  { path: "shopping", component: ShoppingComponent},
+  { path: "inventory", component: InventoryComponent},
+  { path: "statistics", component: StatisticsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,29 +4,57 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonToggleModule} from '@angular/material/button-toggle';
 
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatListModule} from '@angular/material/list';
-import {MatIconModule} from '@angular/material/icon';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
+import { MatSidenavModule} from '@angular/material/sidenav';
+import { MatListModule} from '@angular/material/list';
+import { MatIconModule} from '@angular/material/icon';
+import { MatToolbarModule} from '@angular/material/toolbar';
+import { MatButtonModule} from '@angular/material/button';
+import { HttpClientModule } from '@angular/common/http';
+import { CategorySelectComponent } from './views/components/category-select/category-select.component';
+import { ShoppingComponent } from './views/shopping/shopping.component';
+import { InventoryComponent } from './views/inventory/inventory.component';
+import { StatisticsComponent } from './views/statistics/statistics.component';
+import { MatPaginatorModule} from '@angular/material/paginator';
+import { MatCardModule} from '@angular/material/card';
+
+import { MatInputModule} from '@angular/material/input';
+import { MatFormFieldModule} from '@angular/material/form-field';
+import { FormsModule} from '@angular/forms';
+import { MatDialogModule} from '@angular/material/dialog';
+import { SearchComponent } from './views/components/search/search.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategorySelectComponent,
+    ShoppingComponent,
+    InventoryComponent,
+    StatisticsComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     MatSidenavModule,
     MatListModule,
     MatIconModule,
     MatToolbarModule,
-    MatButtonModule
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatPaginatorModule,
+    MatCardModule,
+    FormsModule, 
+    MatFormFieldModule, 
+    MatInputModule,
+    MatDialogModule
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
