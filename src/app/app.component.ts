f86f6b434c3e8fa7c101e82be9270d5f8fb24667
Zumin Li <zumin.li.work@gmail.com>
2024-11-29 15:10:11 +0800

    1. modify version 2. Add version display 3. Add Jenkinsfile for prod

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ToastService} from "./services/toast.service";
+import packageJson from '../../package.json';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +8,15 @@ import {ToastService} from "./services/toast.service";
   styleUrls: ['./app.component.scss'],
   animations:[]
 })
-export class AppComponent{
+export class AppComponent implements OnInit{
   title = '首页';
+  public version: string = packageJson.version;
+
   constructor(public toastService:ToastService){}
+
+  ngOnInit() {
+    console.log("Current version is " + this.version);
+  }
 
   clear() {
     this.toastService.clear();
